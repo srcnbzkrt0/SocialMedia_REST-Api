d0001f0a8a5d3598cedabf5372f6794a98557056
srcnbzkrt0 <sercan.bozkurt.eem@gmail.com>
2022-08-27 17:17:26 +0300

    Mern Stack Social Media Auth Controller

---
+++ Routes/AuthRoute.js
@@ -0,0 +1,9 @@
+import express from "express";
+import { registerUser } from "../Controllers/AuthController.js";
+
+const router = express.Router()
+
+
+router.post('/register', registerUser)
+
+export default router
